38fc82921e771d428dc0be573dcae021cf11b9e3
Marvyn95 <marvinluyombya@gmail.com>
2025-10-28 19:23:03 +0300

    Update customer retrieval logic to filter by scheme IDs in home route

--- routes.py
+++ routes.py
@@ -32,12 +32,12 @@ def home():
         villages_count = len(villages)
 
     elif user.get("area_id") != None and user.get("scheme_id") == None:
-        customers = list(db.Customers.find({"umbrella_id": user.get("umbrella_id"), "area_id": user.get("area_id")}))
-        customers_count = len(customers)
-        
         schemes = list(db.Schemes.find({"umbrella_id": user.get("umbrella_id"), "area_id": user.get("area_id")}))
         scheme_count = len(schemes)
         scheme_ids = [str(scheme["_id"]) for scheme in schemes]
+
+        customers = list(db.Customers.find({"umbrella_id": user.get("umbrella_id"), "scheme_id": {"$in": scheme_ids}}))
+        customers_count = len(customers)
         
         villages = list(db.Villages.find({"umbrella_id": user.get("umbrella_id"), "scheme_id": {"$in": scheme_ids}}))
         villages_count = len(villages)
